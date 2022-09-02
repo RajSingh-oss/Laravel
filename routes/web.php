ea5bca6211a02bb3c72995c78989e95a2072e8fc
Rahul <uddeshyamaurya61@gmail.com>
2022-09-03 01:43:14 +0530

    fixed some bug
    Just checking for git

--- routes/web.php
+++ routes/web.php
@@ -16,16 +16,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/home',function(){
-    return("This is home page");
-});
-Route::match(['get', 'post'],'/post',function(){
-    return("This is home page of Post");
-});
-
-
-use Illuminate\Http\Request;
-
-Route::get('/user/{id?}', function  ($id =null) {
-    return 'User '.$id;
-});
